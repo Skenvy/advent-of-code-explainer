edd2fa9c0cfeb386c299d7ee3200bf014863c751
Skenvy <17214791+Skenvy@users.noreply.github.com>
2024-12-27 20:01:54 +1100

    experimented with dynamic module creation but didn't work

--- pkg/src/advent_of_code_explainer/puzzles/year_2024/day_1.py
+++ pkg/src/advent_of_code_explainer/puzzles/year_2024/day_1.py
@@ -1,2 +1,5 @@
-def add_two(number):
+def solve_one(number):
+    return number + 1
+
+def solve_two(number):
     return number + 2
--- pkg/tests/test_example.py
+++ pkg/tests/test_example.py
@@ -1,7 +1,10 @@
 # import pytest
 import advent_of_code_explainer as aoce
-
+from sys import modules
 
 def test_function():
     assert aoce.__version__ == "0.0.1"
-    assert aoce.puzzles.year_2024.day_1.add_two(0) == 2
+    assert aoce.puzzles.year_2024.day_1.solve_two(0) == 2
+    for module in modules:
+        print(module)
+    assert aoce.puzzles.year_2019.day_1.solve_two(0) == 2
