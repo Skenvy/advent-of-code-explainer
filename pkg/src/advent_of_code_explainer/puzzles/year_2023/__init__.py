a8dfac94901d10da08074190cea473c83055c639
Skenvy <17214791+Skenvy@users.noreply.github.com>
2024-12-27 20:29:27 +1100

    Confirm the yearly importer of days works across multiple years

---
+++ pkg/src/advent_of_code_explainer/puzzles/year_2023/__init__.py
@@ -0,0 +1,12 @@
+from datetime import datetime, UTC
+from importlib import import_module
+import os
+
+CURRENT_TIME = datetime.now(UTC)
+CONTAINING_YEAR_MODULE = os.path.dirname(os.path.realpath(__file__)).split('/')[-1]
+
+for day in range(1, CURRENT_TIME.day+1):
+    try:
+        import_module(f'.day_{day}', package=f'advent_of_code_explainer.puzzles.{CONTAINING_YEAR_MODULE}')
+    except ModuleNotFoundError:
+        pass
