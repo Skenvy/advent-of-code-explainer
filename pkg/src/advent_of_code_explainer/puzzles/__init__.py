2e8b1d8b6c47766ad811e7de0105c1b7b46e80a2
Skenvy <17214791+Skenvy@users.noreply.github.com>
2024-12-27 20:17:47 +1100

    Have the years and days dynamically imported

--- pkg/src/advent_of_code_explainer/puzzles/__init__.py
+++ pkg/src/advent_of_code_explainer/puzzles/__init__.py
@@ -1,24 +1,11 @@
 from datetime import datetime, UTC
 from importlib import import_module
-from sys import modules
-from types import ModuleType
-
 
 CURRENT_TIME = datetime.now(UTC)
 MOST_RECENT_DECEMBER_YEAR = CURRENT_TIME.year if CURRENT_TIME.month == 12 else CURRENT_TIME.year - 1
-PKG = 'advent_of_code_explainer.puzzles'
 
 for year in range(2015, MOST_RECENT_DECEMBER_YEAR+1):
     try:
-        import_module(f'.year_{year}', package=PKG)
+        import_module(f'.year_{year}', package='advent_of_code_explainer.puzzles')
     except ModuleNotFoundError:
-        modules[f'{PKG}.year_{year}'] = ModuleType(f'{PKG}.year_{year}')
-        for day in range(1, CURRENT_TIME.day+1 if year == MOST_RECENT_DECEMBER_YEAR else 26):
-            modules[f'{PKG}.year_{year}.day_{day}'] = ModuleType(f'{PKG}.year_{year}.day_{day}')
-            def solve_one(*args, **kwargs):
-                raise NotImplementedError
-            def solve_two(*args, **kwargs):
-                raise NotImplementedError
-            modules[f'{PKG}.year_{year}.day_{day}'].solve_one = solve_one
-            modules[f'{PKG}.year_{year}.day_{day}'].solve_two = solve_two
-        import_module(f'{PKG}.year_{year}')
+        pass
--- pkg/tests/test_example.py
+++ pkg/tests/test_example.py
@@ -1,10 +1,7 @@
 # import pytest
 import advent_of_code_explainer as aoce
-from sys import modules
+
 
 def test_function():
     assert aoce.__version__ == "0.0.1"
     assert aoce.puzzles.year_2024.day_1.solve_two(0) == 2
-    for module in modules:
-        print(module)
-    assert aoce.puzzles.year_2019.day_1.solve_two(0) == 2
