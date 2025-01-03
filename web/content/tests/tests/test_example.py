930649a62d05f3eb4544b0db483cc31d59b6c7da
Skenvy <17214791+Skenvy@users.noreply.github.com>
2025-01-03 18:09:16 +1100

    WIP pyscript test passes but requires the function copied in the test's on-click

--- web/content/tests/tests/test_example.py
+++ web/content/tests/tests/test_example.py
@@ -1,11 +1,13 @@
 import asyncio
 from base import upytest
 
+import advent_of_code_explainer as aoce
+
 from pyscript import document, web, when
 
 async def test_example():
-    input_container = web.page.find("#input_number")[0]
-    input_container.innerHTML = "11"
+    input_container = document.querySelector("#input_number")
+    input_container.value = "101"
 
     just_a_button = web.page.find("#do_a_thing_button")[0]
     call_flag = asyncio.Event()
@@ -13,11 +15,16 @@ async def test_example():
     @when("click", just_a_button)
     def on_click(event):
         call_flag.set()
+        input_text = document.querySelector("#input_number")
+        input_number = input_text.value
+        output_div = document.querySelector("#output_number")
+        # The current version this is just a placeholder function that adds 2.
+        output_div.innerText = str(aoce.puzzles.year_2024.day_1.solve_one(int(input_number)))
 
     # Now let's simulate a click on the button (using the low level JS API)
     # so we don't risk dom getting in the way
     just_a_button._dom_element.click()
     await call_flag.wait()
 
-    output_container = web.page.find("#output_number")[0]
-    assert output_container.innerHTML == "12", output_container.innerHTML
+    output_container = document.querySelector("#output_number")
+    assert output_container.innerHTML == "102", output_container.innerHTML
